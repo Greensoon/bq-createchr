784ca5bfe3a53c581cca0fbfd28318bf6b06ce1c
baihuichao <baihuichao@52muyou.com>
2019-06-17 10:54:16 +0800

    代码提交

--- build/background.js
+++ build/background.js
@@ -1,2 +1,14 @@
-const background = require('../src/background.js')
-console.log(background)
+import '../src/background.js'
+function injectCustomJs(jsPath)
+{
+    jsPath = jsPath || 'injects.js';
+    var temp = document.createElement('script');
+    temp.setAttribute('type', 'text/javascript');
+    temp.src = chrome.extension.getURL(jsPath);
+    document.body.appendChild(temp);
+}
+
+import manifest from '../manifest.json'
+if(manifest.injects) {
+    injectCustomJs()
+}
